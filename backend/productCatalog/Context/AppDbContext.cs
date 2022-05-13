6f7565e19a841995ceeccdec529c0f49968eb25b
lexpierin <alex_pierini@hotmail.com>
2022-05-12 22:43:33 -0400

    EF Core context file creation

---
+++ backend/productCatalog/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using productCatalog.Models;
+
+namespace productCatalog.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+}
