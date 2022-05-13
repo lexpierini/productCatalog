c1a8e738409f0abbce06a38d9389be7d8d087058
lexpierin <alex_pierini@hotmail.com>
2022-05-12 23:10:16 -0400

    Service entries in the Program class

--- backend/productCatalog/Program.cs
+++ backend/productCatalog/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using productCatalog.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string postgreSqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(postgreSqlConnection));
 
 var app = builder.Build();
 
