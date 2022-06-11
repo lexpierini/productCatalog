d82a6476ed244fbf9dc7311cca283260ca0b1fae
lexpierin <alex_pierini@hotmail.com>
2022-06-11 11:16:22 -0400

    Simple error handling application

--- backend/productCatalog/Controllers/CategoriesController.cs
+++ backend/productCatalog/Controllers/CategoriesController.cs
@@ -20,69 +20,112 @@ namespace productCatalog.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Category>> GetAll()
         {
-            return _context.Categories.AsNoTracking().ToList();
+            try
+            {
+                return _context.Categories.AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpGet("{id:int}", Name = "GetOneCategory")]
         public ActionResult<Category> GetOne(int id)
         {
-            var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
-
-            if (category is null)
+            try
             {
-                return NotFound("Category not found");
+                var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
+
+                if (category is null)
+                {
+                    return NotFound("Category not found");
+                }
+                return Ok(category);
             }
-            return Ok(category);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
+            
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
         {
-            return _context.Categories.Include(p => p.Products).AsNoTracking().ToList();
+            try
+            {
+                return _context.Categories.Include(p => p.Products).AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpPost]
         public ActionResult AddOne(Category category)
         {
-            if(category is null)
+            try
             {
-                return BadRequest("The data entered is invalid");
+                if (category is null)
+                {
+                    return BadRequest("The data entered is invalid");
+                }
+
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+
+                return new CreatedAtRouteResult("GetOneCategory", new { id = category.CategoryId }, category);
             }
-
-            _context.Categories.Add(category);
-            _context.SaveChanges();
-
-            return new CreatedAtRouteResult("GetOneCategory", new { id = category.CategoryId }, category);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Update(int id, Category category)
         {
-            if (id != category.CategoryId)
+            try
             {
-                return BadRequest("The id informed does not match the id of the category informed");
+                if (id != category.CategoryId)
+                {
+                    return BadRequest("The id informed does not match the id of the category informed");
+                }
+
+                _context.Entry(category).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok(category);
             }
-
-            _context.Entry(category).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok(category);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult DeleteOne(int id)
         {
-            var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
-
-            if (category is null)
+            try
             {
-                return NotFound("Category not found");
+                var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
+
+                if (category is null)
+                {
+                    return NotFound("Category not found");
+                }
+
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+
+                return Ok(category);
             }
-
-            _context.Categories.Remove(category);
-            _context.SaveChanges();
-
-            return Ok(category);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
     }
--- backend/productCatalog/Controllers/ProductsController.cs
+++ backend/productCatalog/Controllers/ProductsController.cs
@@ -19,67 +19,102 @@ namespace productCatalog.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Product>> GetAll()
         {
-            var products = _context.Products.AsNoTracking().ToList();
-            if (products is null)
+            try
             {
-                return NotFound("Products not found");
+                var products = _context.Products.AsNoTracking().ToList();
+                if (products is null)
+                {
+                    return NotFound("Products not found");
+                }
+                return products;
             }
-            return products;
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpGet("{id:int}", Name = "GetOneProduct")]
         public ActionResult<Product> GetOne(int id)
         {
-            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
-            if (product is null)
+            try
             {
-                return NotFound("Products not found");
+                var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+                if (product is null)
+                {
+                    return NotFound("Products not found");
+                }
+                return product;
             }
-            return product;
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpPost]
         public ActionResult AddOne(Product product)
         {
-            if (product is null)
+            try
             {
-                return BadRequest("The data entered is invalid");
+                if (product is null)
+                {
+                    return BadRequest("The data entered is invalid");
+                }
+
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return new CreatedAtRouteResult("GetOneProduct", new { id = product.ProductId }, product);
             }
-
-            _context.Products.Add(product);
-            _context.SaveChanges();
-
-            return new CreatedAtRouteResult("GetOneProduct", new { id = product.ProductId }, product);            
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Update(int id, Product product)
         {
-            if (id != product.ProductId)
+            try
             {
-                return BadRequest("The id informed does not match the id of the product informed");
+                if (id != product.ProductId)
+                {
+                    return BadRequest("The id informed does not match the id of the product informed");
+                }
+
+                _context.Entry(product).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok(product);
             }
-
-            _context.Entry(product).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok(product);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult DeleteOne(int id)
         {
-            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
-
-            if (product is null)
+            try
             {
-                return NotFound("Product not found");
+                var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+                if (product is null)
+                {
+                    return NotFound("Product not found");
+                }
+
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+
+                return Ok(product);
             }
-
-            _context.Products.Remove(product);
-            _context.SaveChanges();
-
-            return Ok(product);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "There was a problem handling your request");
+            }
         }
     }
 }
