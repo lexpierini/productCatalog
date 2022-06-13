1af02cc74f18b9e82c25f9e5dd5fa18c5b13e312
lexpierin <alex_pierini@hotmail.com>
2022-06-12 23:30:36 -0400

    Domain models

---
+++ backendMinimalApi/backendMinimalApi/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace backendMinimalApi.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+
+        // Relationship Properties (1:N)
+        public ICollection<Product>? Products { get; set; }
+    }
+}
