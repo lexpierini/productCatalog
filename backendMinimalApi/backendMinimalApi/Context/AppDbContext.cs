ed80be6d2607e5ab34aae1c5e6fada573bf8c0a5
lexpierin <alex_pierini@hotmail.com>
2022-06-13 21:37:26 -0400

    Configuring Context Properties using Fluent API

--- backendMinimalApi/backendMinimalApi/Context/AppDbContext.cs
+++ backendMinimalApi/backendMinimalApi/Context/AppDbContext.cs
@@ -9,5 +9,20 @@ namespace backendMinimalApi.Context
 
         public DbSet<Product>? Products { get; set; }
         public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasKey(c => c.CategoryId);
+            mb.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            mb.Entity<Product>().HasKey(p => p.ProductId);
+            mb.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+            mb.Entity<Product>().Property(p => p.Description).HasMaxLength(150);
+            mb.Entity<Product>().Property(p => p.ImageUrl).HasMaxLength(100);
+            mb.Entity<Product>().Property(p => p.Price).HasPrecision(14, 2);
+
+            // Relationship
+            mb.Entity<Product>().HasOne<Category>(c => c.Category).WithMany(p => p.Products).HasForeignKey(c => c.CategoryId);
+        }
     }
 }
