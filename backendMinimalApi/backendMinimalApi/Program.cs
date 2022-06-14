9fad9d84b0693b503ceaa2b2b69f3f7f507de8f6
lexpierin <alex_pierini@hotmail.com>
2022-06-13 21:50:27 -0400

    PostgreSQL connection settings

--- backendMinimalApi/backendMinimalApi/Program.cs
+++ backendMinimalApi/backendMinimalApi/Program.cs
@@ -1,9 +1,20 @@
+using backendMinimalApi.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. = ConfigureServices (startup)
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// PostgreSQL connection settings -------------------------------------------------------- //
+string postgreSqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(postgreSqlConnection));
+// --------------------------------------------------------------------------------------- //
 
 var app = builder.Build();
 
@@ -14,30 +25,4 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
