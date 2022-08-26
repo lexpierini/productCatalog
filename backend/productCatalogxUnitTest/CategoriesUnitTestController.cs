fc48a2fb4dfc4abde6dfe6bbaabfbccfe12dc3a9
lexpierin <alex_pierini@hotmail.com>
2022-08-26 00:01:45 -0400

    Documentation and Testing

---
+++ backend/productCatalogxUnitTest/CategoriesUnitTestController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using productCatalog.Context;
+using productCatalog.Controllers;
+using productCatalog.DTOs;
+using productCatalog.DTOs.Mappings;
+using productCatalog.Pagination;
+using productCatalog.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productCatalogxUnitTest
+{
+    public class CategoriesUnitTestController
+    {
+
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork repository;
+
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        public static string connectionStrings = "User ID=dev;Password=dev123;Host=localhost;Port=5432;Database=CatalogDB;";
+
+        static CategoriesUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql(connectionStrings)
+                .Options;
+        }
+
+        public CategoriesUnitTestController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            mapper = config.CreateMapper();
+
+            var context = new AppDbContext(dbContextOptions);
+
+            repository = new UnitOfWork(context);
+        }
+
+        // Unit test
+        // Test the GET method
+        [Fact]
+        public async void GetCategories_Return_OkResult()
+        {
+            //Arrange
+            var controller = new CategoriesController(repository, mapper);
+            CategoriesParameters categoriesParameters = new CategoriesParameters()
+            {
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            //Act
+            var data = await controller.GetAll(categoriesParameters);
+
+            //Assert
+            Assert.IsType<CategoryDTO>(data.Value.First());
+        }
+
+    }
+}
---
+++ backend/productCatalogxUnitTest/DBUnitTestsMockInitializer.cs
@@ -0,0 +1,39 @@
+﻿using productCatalog.Context;
+using productCatalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productCatalogxUnitTest
+{
+    public  class DBUnitTestsMockInitializer
+    {
+        public DBUnitTestsMockInitializer() { }
+
+        public void Seed(AppDbContext context)
+        {
+            context.Categories.Add
+                (new Category { CategoryId = 999, Name = "Drink999", ImageUrl = "drink999.jpg" });
+
+            context.Categories.Add
+                (new Category { CategoryId = 2, Name = "Juice", ImageUrl = "juice2.jpg" });
+
+            context.Categories.Add
+                (new Category { CategoryId = 3, Name = "Candy777", ImageUrl = "candy3.jpg" });
+
+            context.Categories.Add
+                (new Category { CategoryId = 4, Name = "Snacks", ImageUrl = "snacks4.jpg" });
+
+            context.Categories.Add
+                (new Category { CategoryId = 5, Name = "Pie", ImageUrl = "pie5.jpg" });
+
+            context.Categories.Add
+                (new Category { CategoryId = 6, Name = "Cake", ImageUrl = "cake6.jpg" });
+
+            context.SaveChanges();
+
+        }
+    }
+}
